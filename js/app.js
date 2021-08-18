92d017c010fd7663cf53a65307c807971c79380f
Ali Ajeli Lahiji <mr.ali.ajeli@hotmail.com>
2021-08-18 14:11:33 +0430

    Final Version

--- js/app.js
+++ js/app.js
@@ -14,37 +14,45 @@ const btnBuy = document.querySelector("#btn-buy");
 const btnSupport = document.querySelector("#btn-support");
 const btnCustomers = document.querySelector("#btn-customers");
 const btnContact = document.querySelector("#btn-contact");
+const btnContact2 = document.querySelector("#btn-contact-2");
 const btnAbout = document.querySelector("#btn-about");
 
 const sections = document.querySelectorAll("section");
 
-const showHide = function (section) {
+const showHide = function (section, btn) {
 	sections.forEach((sec) => sec.classList.add("hidden"));
 	section.classList.remove("hidden");
+	[btnApps, btnDownloads, btnBuy, btnSupport, btnCustomers, btnContact, btnAbout].forEach((btn) => btn.classList.remove("app__nav-link--active"));
+	if (btn === "no") return;
+	btn.classList.add("app__nav-link--active");
 };
 btnHome.addEventListener("click", function () {
-	showHide(home);
+	showHide(home, "no");
 });
+
 btnApps.addEventListener("click", function () {
-	showHide(apps);
+	showHide(apps, btnApps);
 });
 btnDownloads.addEventListener("click", function () {
-	showHide(downloads);
+	showHide(downloads, btnDownloads);
 });
 btnBuy.addEventListener("click", function () {
-	showHide(buy);
+	showHide(buy, btnBuy);
 });
 btnSupport.addEventListener("click", function () {
-	showHide(support);
+	showHide(support, btnSupport);
 });
 btnCustomers.addEventListener("click", function () {
-	showHide(customers);
+	showHide(customers, btnCustomers);
 });
 btnContact.addEventListener("click", function () {
-	showHide(contactUs);
+	showHide(contactUs, btnContact);
+});
+btnContact2.addEventListener("click", function () {
+	showHide(contactUs, btnContact);
 });
 btnAbout.addEventListener("click", function () {
-	showHide(aboutUs);
+	showHide(aboutUs, btnAbout);
 });
 
 // ------------------------------------------------------------------------------
@@ -53,6 +61,18 @@ const btnCity = document.querySelectorAll(".btn-city");
 btnCity.forEach((city) => {
 	city.addEventListener("click", function () {
 		this.parentNode.classList.toggle("grid-up");
-		console.log(city);
 	});
+});
+
+//-----------------------------------------------------------------------------
+const btnEdu = document.querySelector("#btn-edu");
+const btnEduExit = document.querySelector("#btn-edu-exit");
+const eductions = document.querySelector(".downloads__eductions");
+
+btnEdu.addEventListener("click", function () {
+	eductions.classList.add("show");
+});
+
+btnEduExit.addEventListener("click", function () {
+	eductions.classList.remove("show");
 });
